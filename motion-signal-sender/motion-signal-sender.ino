db077a114a9bcc825e76992b818eb890040b9e52
faustocv <fausto.castaneda.v@gmail.com>
2016-05-23 18:23:53 -0500

    Adding sender and trigger about motion

--- motion-signal-sender/motion-signal-sender.ino
+++ motion-signal-sender/motion-signal-sender.ino
@@ -1,8 +1,10 @@
 const int ledPin= 13;
+const int espPin= 12;
 const int inputPin= 4;
 
 void setup(){
  pinMode(ledPin, OUTPUT);
+ pinMode(espPin, OUTPUT);
  pinMode(inputPin, INPUT);
 }
 
@@ -12,11 +14,14 @@ void loop(){
  if (value == HIGH)
  {
   digitalWrite(ledPin, HIGH);
-  delay(60000);
+  digitalWrite(espPin, HIGH);
+  delay(600);
   digitalWrite(ledPin, LOW);
+  digitalWrite(espPin, LOW);
  }
  else
  {
   digitalWrite(ledPin, LOW);
+  digitalWrite(espPin, LOW);
  }
 }
