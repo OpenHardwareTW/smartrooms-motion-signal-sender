f56ad5c107f8c05208a106f62fb89d89b16cc2c7
eguezgustavo <correo@gustavoeguez.com>
2016-09-12 08:41:55 -0500

    changes topic to room name

--- raspbery_pi/production/python_scripts/mqtt.py
+++ raspbery_pi/production/python_scripts/mqtt.py
@@ -5,9 +5,10 @@ class MQTT():
     def __init__(self, server, port, room_name):
         self.server = server
         self.port = port
+        self.topic = room_name
         self.client = mqtt.Client(room_name)
         self.client.connect(self.server, self.port)
 
-    def publish(self, message, topic):
-        self.client.publish(topic, message)
+    def publish(self, message):
+        self.client.publish(self.room_name, message)
         self.client.loop(2)
